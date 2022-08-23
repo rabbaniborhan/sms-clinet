21d052bac0fb7bfe603058921dada2e27496de1d
mdhshawon69 <mahmudulhaqshaown@gmail.com>
2022-08-23 18:34:38 +0600

    Add more text on header  --> Done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,6 +13,7 @@ const Navbar = ({ navPath, setShowModal }) => {
   const handleScroll = () => {
     if (window.scrollY >= 30) {
       setBackground(true);
+      setShowModal(false);
     } else {
       setBackground(false);
     }
