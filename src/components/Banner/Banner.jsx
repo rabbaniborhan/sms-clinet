48ec8b88291b19444c4aa794d54af43fdfd25c69
mdhshawon69 <mahmudulhaqshaown@gmail.com>
2022-11-14 20:27:47 +0600

    Make Banner component Responsive --> Done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -9,6 +9,8 @@ import MobileNavbar from "../MobileNavbar/MobileNavbar";
 // <---------------The parent component is index.jsx/Homepage + teacher-info.jsx + staff-info.jsx + about-us.jsx + contact.jsx---------------------->
 
 const Banner = ({ path, navPath, subPath, setShowModal, paddingAlter }) => {
+  const mobileWidth =
+    typeof window !== "undefined" ? window.innerWidth < 769 : null;
   const dispatch = useDispatch();
 
   const handleShowModal = () => {
@@ -25,7 +27,7 @@ const Banner = ({ path, navPath, subPath, setShowModal, paddingAlter }) => {
         {/* Conditionally setting the height of the banner for Homepage and also for other pages */}
 
         <div
-          className={`flex justify-between items-end lg:pb-24 xl:pb-40 banner-height relative ${
+          className={`flex lg:flex-row flex-col justify-between items-end lg:pb-24 xl:pb-40 banner-height lg:relative text-center lg:text-left ${
             path.heightAlter ? `lg:h-[65vh]` : `lg:banner-height`
           } w-full`}>
           <div className='text-white'>
@@ -51,10 +53,10 @@ const Banner = ({ path, navPath, subPath, setShowModal, paddingAlter }) => {
                 className={`${
                   !path.paddingAlter ? "pb-8" : "pb-4"
                 } xl:space-y-10`}>
-                <h1 className='lg:text-5xl font-bold xl:text-7xl mb-7 font-bangla'>
+                <h1 className='lg:text-5xl text-3xl font-bold xl:text-7xl lg:mb-7 mb-4 mt-7 font-bangla'>
                   পূর্ব রামপুরা স্কুল এন্ড কলেজ
                 </h1>
-                <p className='w-3/5 lg:leading-[30px] xl:leading-[40px] xl:text-2xl font-bangla'>
+                <p className='lg:w-3/5 w-10/12 mx-auto lg:mx-0 lg:leading-[30px] xl:leading-[40px] xl:text-2xl font-bangla'>
                   যে কথাকে কাজে লাগাতে চাও, তাকে কাজে লাগানোর কথা চিন্তা করার
                   আগে ভাবো, তুমি কি সেই কথার জাদুতে আচ্ছন্ন হয়ে গেছ কিনা তুমি
                   যদি নিশ্চিত হও যে, তুমি কোনো মোহাচ্ছাদিত আবহে যে কথাকে কাজে
@@ -63,9 +65,9 @@ const Banner = ({ path, navPath, subPath, setShowModal, paddingAlter }) => {
                   যে, তুমি কোনো মোহাচ্ছাদিত আবহে
                 </p>
                 {path.btn && (
-                  <div className='space-x-3 mt-7 xl:text-2xl'>
+                  <div className='space-x-3 lg:mt-7 mt-5 xl:text-2xl'>
                     <button
-                      className='lg:px-8 lg:py-2 xl:px-16 xl:py-4 rounded-sm outline-none bg-yellow'
+                      className='px-7 py-2 lg:px-8 lg:py-2 xl:px-16 xl:py-4 rounded-sm outline-none bg-yellow'
                       onClick={handleShowModal}>
                       Log in
                     </button>
@@ -81,16 +83,16 @@ const Banner = ({ path, navPath, subPath, setShowModal, paddingAlter }) => {
           {/* Applying image width and height as per the page url passed in the props. It applies different image for Homepage and other pages. Also conditionally applying image for staff-info and teacher-info page */}
           {!path.imageAlter ? (
             <div
-              className={`absolute ${
+              className={`lg:absolute ${
                 path.heightAlter
                   ? "lg:h-[350px] lg:w-[230px] lg:-mb-24 xl:h-[550px] xl:w-[360px] xl:-mb-40 xl:right-20 lg:right-32"
-                  : "lg:h-[85%] lg:w-[27%] xl:h-[800px] xl:w-[550px] xl:right-0 lg:right-16 lg:-mb-24 xl:-mb-40"
+                  : "h-auto w-[75%] mx-auto lg:h-[85%] lg:w-[27%] xl:h-[700px] xl:w-[550px] xl:right-0 lg:right-16 lg:-mb-24 xl:-mb-40"
               }`}>
               <Image
                 src={images.headerImage}
                 alt='female-student-holding-books'
-                layout='fill'
                 objectFit='cover'
+                layout={mobileWidth === false ? "fill" : undefined}
               />
             </div>
           ) : (
