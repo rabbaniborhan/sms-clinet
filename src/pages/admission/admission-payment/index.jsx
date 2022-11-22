ffee57b0f10909d842a0168fbc15c42314f8cb83
mdhshawon69 <mahmudulhaqshaown@gmail.com>
2022-11-22 21:06:24 +0600

    Modifie pages --> Done

--- src/pages/admission/admission-payment/index.jsx
+++ src/pages/admission/admission-payment/index.jsx
@@ -6,6 +6,9 @@ import AdmissionPaymentSlip from "./AdmissionPaymentSlip";
 const AdmissionPayment = () => {
   return (
     <LayoutSecond>
+      <head>
+        <meta name='viewport' content='width=1280'></meta>
+      </head>
       <div className='lg:w-4/5 w-auto mx-auto my-20 py-20'>
         <AdmissionPaymentSlip />
       </div>
